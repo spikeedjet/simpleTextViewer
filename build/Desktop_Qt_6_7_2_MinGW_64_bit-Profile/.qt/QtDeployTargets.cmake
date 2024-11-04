aa2d88c61480274ba339ff668faf85f157b9914e
spikeedjet <104442518+spikeedjet@users.noreply.github.com>
2024-11-04 16:23:56 +0800

    first commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Profile/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_simpletextviewer_FILE E:/QT/Examples/Qt-6.7.2/assistant/simpletextviewer/build/Desktop_Qt_6_7_2_MinGW_64_bit-Profile/simpletextviewer.exe)
+set(__QT_DEPLOY_TARGET_simpletextviewer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_simpletextviewer_RUNTIME_DLLS E:/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;E:/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;E:/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
